f9909f4c5b22184146bcfc5906e9bbed92f52c1b
smithsf0x <simonschmidt1888@gmail.com>
2016-08-25 16:02:49 +0200

    added travis build script

---
+++ .travis_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PSVSDK_URL="https://bintray.com/vitasdk/vitasdk/download_file?file_path=vitasdk-gcc-5.4-linux-compat-nightly-714786ee6e105972d6a78e912e4d1de52718dafe.tar.bz2"
+
+function getSDK {
+  echo "Download SDK:"
+  curl "$PSVSDK_URL" > sdk.tar.bz2
+  echo "Donwload Finished"
+
+  echo "untar and unzip"
+  mkdir psvsdk && tar jxf sdk.tar.bz2 -C psvsdk
+  echo "decompressing finished"
+  ls psvsdk
+}
